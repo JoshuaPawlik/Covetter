a20987e2953683ccaba3fafd628e7cebcdbf1161
joshuapawlik <joshawesome12@yahoo.com>
2018-09-26 18:18:13 -0500

    Grabs value of textfields and stores that into to db rather than state

--- src/app.js
+++ src/app.js
@@ -12,8 +12,7 @@ class App extends React.Component {
   constructor(props){
     super(props)
     this.state = {
-      mode: "editing",
-      id:"",
+      id:null,
       title: "",
       par1: "",
       par2: "",
@@ -24,7 +23,6 @@ class App extends React.Component {
 
   // TODO: Add functions for:
   //new/create file
-  //Preview whole
   //Add Paragraph
   //Delete file
   //Add alternate paragraph
@@ -35,14 +33,25 @@ class App extends React.Component {
   //Make sure database works correctly when packaging with asar again
 
   newFile = () => {
+    //clear all textfields
+    //make sure you're not editing another existing file
+  }
 
+
+  setActiveFile = () => {
+    //set all textfield values
+    // make sure all changes are updated to active file
   }
 
   onSave = () =>{
-    let title = this.state.title;
-    let par1 = this.state.par1;
-    let par2 = this.state.par2;
-    let par3 = this.state.par3;
+    // let title = this.state.title;
+    // let par1 = this.state.par1;
+    // let par2 = this.state.par2;
+    // let par3 = this.state.par3;
+    let title = document.getElementById('title').value;
+    let par1 = document.getElementById('par1').innerHTML;
+    let par2 = document.getElementById('par2').innerHTML;
+    let par3 = document.getElementById('par3').innerHTML;
     //Checks to make sure Title is not empty
     if (title === ""){
       this.setState({titleClass:"title red-shake"})
@@ -64,11 +73,6 @@ class App extends React.Component {
     //retrieve files
     this.getFiles()
   }
-
-  preview = () => {
-    this.setState({mode:"preview"})
-  }
-
 
   //This function is mostly to prevent
   //unwanted actions
@@ -112,7 +116,8 @@ class App extends React.Component {
 
 
   handleClick = () => {
-    console.log('clicked');
+    document.getElementById('par1').innerHTML = "Changed value"
+    // console.log('clicked');
   }
 
   onFileClick = (title) => {
@@ -138,15 +143,14 @@ class App extends React.Component {
         <div className="sidebar">
           <FontAwesomeIcon icon={faHome} className="home-icon" onClick={this.handleClick.bind(this)} />
           <FontAwesomeIcon icon={faSave} className="home-icon" onClick={this.onSave.bind(this)} />
-          <FontAwesomeIcon icon={faPlusSquare} className="home-icon" onClick={this.handleClick.bind(this)} />
+          <FontAwesomeIcon icon={faPlusSquare} className="home-icon" onClick={this.newFile.bind(this)} />
         </div>
         <div className="writingCom">
-          {this.state.mode === 'editing' &&
           <Writing
             state={this.state}
             keyUpUpdate={this.keyUpUpdate.bind(this)}  keyDownUpdate={this.keyDownUpdate.bind(this)} handleClick={this.handleClick.bind(this)}
             updateTitle={this.updateTitle.bind(this)}
-          />}
+          />
 
           <div className='prevContainer'>
             {this.state.files ? <Files onFileClick={this.onFileClick.bind(this)} files={this.state.files}/>: null}
--- src/writing.js
+++ src/writing.js
@@ -13,28 +13,28 @@ class Writing extends React.Component {
       <div className="writing">
         <div className="content">
           <div className="titleDiv">
-            <input type='text' placeholder={"Title"} className={this.props.state.titleClass} onChange={(e) => this.props.updateTitle(e)}></input>
+            <input type='text' id="title" placeholder={"Title"} className={this.props.state.titleClass} onChange={(e) => this.props.updateTitle(e)}></input>
           </div>
           <div className='paragraphs'>
             <div className="paragraph">
               {/* Number */}
               <h3 className="p-num" onClick={this.props.handleClick}>1</h3>
               {/* Textfield */}
-              <div contentEditable type='text' className="textarea" placeholder="Paragraph 1" onKeyUp={(e) => this.props.keyUpUpdate(e,1)} onKeyDown={this.props.keyDownUpdate}></div>
+              <div contentEditable type='text' id="par1" className="textarea" placeholder="Paragraph 1" onKeyUp={(e) => this.props.keyUpUpdate(e,1)} onKeyDown={this.props.keyDownUpdate}></div>
 
             </div>
             <div className="paragraph">
               {/* Number */}
               <h3 className="p-num" onClick={this.props.handleClick}>2</h3>
               {/* Textfield */}
-              <div contentEditable type='text' className="textarea" placeholder="Paragraph 2" onKeyUp={(e) => this.props.keyUpUpdate(e,2)} onKeyDown={ this.props.keyDownUpdate}></div>
+              <div contentEditable type='text' id="par2" className="textarea" placeholder="Paragraph 2" onKeyUp={(e) => this.props.keyUpUpdate(e,2)} onKeyDown={ this.props.keyDownUpdate}></div>
 
             </div>
             <div className="paragraph">
               {/* Number */}
               <h3 className="p-num" onClick={this.props.handleClick}>3</h3>
               {/* Textfield */}
-              <div contentEditable type='text' className="textarea" placeholder="Paragraph 3" onKeyUp={(e) => this.props.keyUpUpdate(e,3)} onKeyDown={ this.props.keyDownUpdate}></div>
+              <div contentEditable type='text' id="par3" className="textarea" placeholder="Paragraph 3" onKeyUp={(e) => this.props.keyUpUpdate(e,3)} onKeyDown={ this.props.keyDownUpdate}></div>
 
             </div>
           </div>
